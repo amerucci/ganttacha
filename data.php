ba20d04d778585fce45ddc87d7dd8acf277f4ab3
amerucci <a.merucci@onlineformapro.com>
2023-01-14 00:47:40 +0100

    Styling Gant Calendar

--- data.php
+++ data.php
@@ -10,9 +10,7 @@ $password = "root";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=ganttacha", $username, $password);
-  // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
@@ -59,51 +57,4 @@ foreach ($results as $result){
   );
 }
 
-var_dump($data);
-
-
-
-
-// $data[] = array(
-//   'label' => 'SAMS',
-//   'formateur' => '',
-//   'start' => '2022-11-21', 
-//   'end'   => '2023-05-24',
-//   'ville' => 'Lons le Saunier'
-// );
-
-// $data[] = array(
-//   'label' => 'MTI',
-//   'formateur' => 'Laurent Ricard',
-//   'start' => '2022-11-21', 
-//   'end'   => '2023-02-10',
-//   'ville' => 'Lons le Saunier'
-// );
-
-// $data[] = array(
-//   'label' => 'COMPÉTENCES-TR + CODE ROUTE',
-//   'formateur' => 'Laurent Ricard',
-//   'start' => '2022-12-12', 
-//   'end'   => '2023-02-23',
-//   'ville' => 'Saint-Claude'
-// );
-
-// $data[] = array(
-//   'label' => 'GEST-COMPT',
-//   'formateur' => '',
-//   'start' => '2022-10-24', 
-//   'end'   => '2023-04-06',
-//   'ville' => 'Dole'
-// );
-
-
-
-
-//var_dump($data);
-// $data[] = array(
-//   'label' => 'Project 4',
-//   'start' => '2012-05-06', 
-//   'end'   => '2012-06-17',
-//   'class' => 'important',
-// );
 ?>
